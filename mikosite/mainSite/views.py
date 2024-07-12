74a82b2ea378bf726670a2545a25d810d66e4bf9
xAlpaca <alermgm@gmail.com>
2024-07-12 17:33:33 +0200

    Home page to phone scaling.

--- mikosite/mainSite/views.py
+++ mikosite/mainSite/views.py
@@ -1,15 +1,53 @@
 from django.shortcuts import render
 from mainSite.models import Post
 from django.http import HttpResponse
+from kolomat.models import Kolo
+from datetime import datetime, date, time
 
 # Create your views here.
 
 
 def index(request):
 
+    today = date.today()
+    now = datetime.now().time()
+
+    # Get all future Kolo instances
+
+    future_kolos = [
+        kolo for kolo in Kolo.objects.all()
+        if kolo.date and kolo.time and
+           (kolo.date > today or (kolo.date == today and kolo.time > now))
+    ]
+
+    if not future_kolos:
+        next_kolo_instances = []
+    else:
+
+        # Sort the future Kolos by date and time
+        future_kolos.sort(key=lambda k: datetime.combine(k.date, k.time))
+
+        # Get the earliest date
+        next_date = future_kolos[0].date
+
+        # Filter instances happening on the same day as the earliest instance
+        next_kolo_instances = [
+            kolo for kolo in future_kolos if kolo.date == next_date
+        ]
+
+        next_kolo_instances = [
+            kolo for kolo in future_kolos if kolo.date == next_date
+        ]
+
+        for i in next_kolo_instances:
+            print(i)
+
+
+
     posts = Post.objects.all()
     reversed_posts = reversed(posts)
-    context = {"posts": reversed_posts}
+    context = {"posts": reversed_posts, "eventy" : next_kolo_instances}
+
 
     context["user"] = request.user
     return render(request, "index.html", context)
