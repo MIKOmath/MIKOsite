c13d392dae7e21dce14e3bc4b0f01d2c95f2d4d3
Karol Musieliński <musielinskikarol@gmail.com>
2025-09-06 21:11:41 +0200

    Make superuser visible in admin panel

--- mikosite/accounts/admin.py
+++ mikosite/accounts/admin.py
@@ -3,9 +3,9 @@ from .models import User, LinkedAccount
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'full_name', 'is_staff')
+    list_display = ('username', 'full_name', 'is_staff', 'is_superuser')
     search_fields = ('username', 'name', 'surname', 'email')
-    ordering = ('-is_staff',)
+    ordering = ('-is_superuser', '-is_staff')
 
 admin.site.register(User, UserAdmin)
 admin.site.register(LinkedAccount)
