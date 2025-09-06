e19bb2cebc86df48f36755531ba6c28283bc0d67
fimpro <filipmanijak05@gmail.com>
2025-09-06 14:28:53 +0200

    Split Homepage from News page

--- mikosite/mainSite/urls.py
+++ mikosite/mainSite/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
+    path("news/", views.news, name="news"),
     # path("roadmap/", views.roadmap, name="roadmap"),
 ]
