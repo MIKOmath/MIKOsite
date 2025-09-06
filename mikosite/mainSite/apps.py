ddd31b7e5a744d1562c9a80f1da21807337966e2
Shadov15 <bar.vik05@gmail.com>
2025-09-06 17:31:46 +0200

    News page upgrade

--- mikosite/mainSite/apps.py
+++ mikosite/mainSite/apps.py
@@ -7,11 +7,6 @@ class MainsiteConfig(AppConfig):
 
     def ready(self):
         from .models import Tag
-        default_tags = [
-            ('matematyka', 'matematyka'),
-            ('informatyka', 'informatyka'),
-            ('AI', 'AI'),
-            ('fizyka', 'fizyka'),
-        ]
-        for name, _ in default_tags:
+        default_tags = ['matematyka', 'informatyka', 'AI', 'fizyka']
+        for name in default_tags:
             Tag.objects.get_or_create(name=name)
