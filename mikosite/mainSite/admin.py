46290cf4241b42e35c226284bb3c58dea7203550
Karol Musieliński <musielinskikarol@gmail.com>
2026-03-08 23:07:05 +0100

    Show open event registration on main page
    As MIKO's operating model is shifting to more in-person events, there is
    growing demand for marketing these events to our community.
    
    This commit introduces a new Django model of an (in-person) event,
    containing the data relevant to registration. The event with active
    registration and closest registration closing time will be displayed on the
    main page to promote it throughout the community.
    
    Additionally, existing call-to-action buttons were restyled slightly.

--- mikosite/mainSite/admin.py
+++ mikosite/mainSite/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
-from .models import Post, Image
 
-admin.site.register(Post)
-admin.site.register(Image)
+from .models import RegistrationEvent, Image, Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "date", "time")
+    search_fields = ("title", "subtitle", "content")
+    ordering = ("-date", "-time")
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ("id", "image")
+
+
+@admin.register(RegistrationEvent)
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "location",
+        "date_begin",
+        "date_end",
+        "registration_begin",
+        "registration_end",
+    )
+    search_fields = ("name", "location")
+    list_filter = ("date_begin", "date_end", "registration_begin", "registration_end", "location")
+    ordering = ("-registration_end", "-date_begin", "name")
--- mikosite/mainSite/models.py
+++ mikosite/mainSite/models.py
@@ -1,14 +1,15 @@
 import os
+
 from babel.dates import format_date, format_time
 from markdown import Markdown
 
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.safestring import mark_safe
 
 from accounts.models import User
 from mainSite.markdown import DisallowHeadersExtension
-
 
 md = Markdown(extensions=[DisallowHeadersExtension()])
 
@@ -51,3 +52,52 @@ class Image(models.Model):
 
     def __str__(self):
         return str(self.image)
+
+
+class RegistrationEvent(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
+    date_begin = models.DateField()
+    date_end = models.DateField()
+    registration_begin = models.DateField()
+    registration_end = models.DateField()
+    registration_url = models.URLField(max_length=500)
+
+    def __str__(self):
+        return f"{self.name} ({self.date_begin} - {self.date_end})"
+
+    def clean(self):
+        errors = {}
+
+        if self.date_end < self.date_begin:
+            errors["date_end"] = "Data zakończenia wydarzenia nie może być wcześniejsza niż data rozpoczęcia."
+
+        if self.registration_end < self.registration_begin:
+            errors["registration_end"] = "Data końca rejestracji nie może być wcześniejsza niż data początku rejestracji."
+
+        if errors:
+            raise ValidationError(errors)
+
+    def format_date_range(self, locale=settings.BABEL_LOCALE) -> str:
+        if self.date_begin == self.date_end:
+            return format_date(self.date_begin, format='d MMMM', locale=locale)
+
+        same_year = self.date_begin.year == self.date_end.year
+        same_month = same_year and self.date_begin.month == self.date_end.month
+
+        if same_month:
+            start_day = format_date(self.date_begin, format='d', locale=locale)
+            end_label = format_date(self.date_end, format='d MMMM', locale=locale)
+            return f"{start_day}-{end_label}"
+
+        start_label = format_date(self.date_begin, format='d MMMM', locale=locale)
+        end_label = format_date(self.date_end, format='d MMMM', locale=locale)
+        return f"{start_label} - {end_label}"
+
+    def display_dict(self, locale=settings.BABEL_LOCALE) -> dict:
+        return {
+            "name": self.name,
+            "location": self.location,
+            "date_range": self.format_date_range(locale=locale),
+            "registration_link": self.registration_url,
+        }
