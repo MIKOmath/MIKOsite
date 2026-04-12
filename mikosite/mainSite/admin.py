1ad8ff3bbc6be68f93714f31bec9a294f86a0568
Karol Musieliński <musielinskikarol@gmail.com>
2026-04-12 02:34:17 +0200

    Improve admin panel UX
    Seminars and posts data will be shown to the editor in a more readable
    format. Easier filtering has been enabled.
    
    Moreover, tutors/authors are now autocomplete fields, making it easier
    to find the right user to assign the object to.

--- mikosite/mainSite/admin.py
+++ mikosite/mainSite/admin.py
@@ -1,13 +1,38 @@
 from django.contrib import admin
+from rangefilter.filters import DateRangeFilterBuilder
+from more_admin_filters import MultiSelectRelatedOnlyFilter
 
 from .models import RegistrationEvent, Image, Post
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("title", "date", "time")
-    search_fields = ("title", "subtitle", "content")
+    autocomplete_fields = ("authors",)
+    list_display = (
+        "title",
+        "date",
+        "author_list",
+    )
+    search_fields = (
+        "title",
+        "subtitle",
+        "authors__username",
+        "authors__last_name",
+        "authors__email",
+    )
+    list_filter = (
+        ("date", DateRangeFilterBuilder(title="date")),
+        ("authors", MultiSelectRelatedOnlyFilter),
+    )
     ordering = ("-date", "-time")
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.prefetch_related("authors")
+
+    @admin.display(description="Authors")
+    def author_list(self, obj):
+        return ", ".join(obj.authors.values_list("username", flat=True)) or "-"
 
 
 @admin.register(Image)
